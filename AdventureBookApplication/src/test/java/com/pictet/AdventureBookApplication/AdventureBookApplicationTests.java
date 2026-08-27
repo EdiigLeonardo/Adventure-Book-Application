bd82c495c0331a1c2f478ca7900f01c6065df0ce
EdiigLeonardo <ediigmelchiior@gmail.com>
2026-08-27 02:41:18 +0100

    chore: bootstrap app config and smoke test

--- AdventureBookApplication/src/test/java/com/pictet/AdventureBookApplication/AdventureBookApplicationTests.java
+++ AdventureBookApplication/src/test/java/com/pictet/AdventureBookApplication/AdventureBookApplicationTests.java
@@ -1,13 +1,21 @@
 package com.pictet.AdventureBookApplication;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.pictet.AdventureBookApplication.service.GameEngineService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AdventureBookApplicationTests {
 
+	@Autowired
+	private GameEngineService gameEngineService;
+
 	@Test
 	void contextLoads() {
+		assertThat(gameEngineService).isNotNull();
 	}
 
 }
